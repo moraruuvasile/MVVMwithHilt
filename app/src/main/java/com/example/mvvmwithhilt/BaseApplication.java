740eacb7e7ef883c0331d60942e23a1bba36fe9b
Vasile <vaseoa@yahoo.com>
2020-07-04 10:34:15 +0300

    start View Binding

--- app/src/main/java/com/example/mvvmwithhilt/BaseApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.mvvmwithhilt;
-
-import android.app.Application;
-
-import dagger.hilt.android.HiltAndroidApp;
-
-@HiltAndroidApp
-public class BaseApplication extends Application {
-}
